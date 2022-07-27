2cd01993a1d9cbd256ee54cf6c63d78fc7fa09ae
The127 <julian.baehr@googlemail.com>
2022-07-27 21:39:25 +0200

    add underline to active navlinks

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -8,12 +8,12 @@ export default function NavBar(){
         <div class="flex flex-row px-16 py-6 sticky top-0 flex-wrap justify-between items-baseline">
             <div class="flex flex-wrap flex-row items-baseline">
                 <Text class="text-3xl pr-8 pb-3">Karolin Bähr</Text>
-                <Text class="text-2xl pr-8 text-gray-500 pb-2">I'm a <i class="text-gray-700">professional</i> software developer</Text>
+                <Text class="text-2xl pr-8 b-2 text-gray-500p">I'm a <i class="text-gray-700">professional</i> software developer</Text>
             </div>
             <div class="flex flex-row pb-2 pr-8">
-                <NavLink class="pr-3 text-xl" end="true" href="/">Home</NavLink>
-                <NavLink class="px-3 text-xl" end="true" href="/portfolio">Portfolio</NavLink>
-                <NavLink class="pl-3 text-xl" end="true" href="/contacts">Contacts</NavLink>
+                <NavLink class="mr-3 text-xl" end="true" href="/">Home</NavLink>
+                <NavLink class="mx-3 text-xl" end="true" href="/portfolio">Portfolio</NavLink>
+                <NavLink class="ml-3 text-xl" end="true" href="/contacts">Contacts</NavLink>
             </div>
             <div class="flex flex-row self-center">
                 <NavLink href="https://github.com/The127" target="blank"><Text class="text-3xl"><Fa icon={faGithub} /></Text></NavLink>
