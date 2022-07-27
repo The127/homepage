f5e5582082576abca4f01e551444fa3e4991f3d5
The127 <julian.baehr@googlemail.com>
2022-07-27 21:29:48 +0200

    make navbar navlinks match route exactly

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -11,9 +11,9 @@ export default function NavBar(){
                 <Text class="text-2xl pr-8 text-gray-500 pb-2">I'm a <i class="text-gray-700">professional</i> software developer</Text>
             </div>
             <div class="flex flex-row pb-2 pr-8">
-                <NavLink class="pr-3 text-xl" href="/">Home</NavLink>
-                <NavLink class="px-3 text-xl" href="/portfolio">Portfolio</NavLink>
-                <NavLink class="pl-3 text-xl" href="/contacts">Contacts</NavLink>
+                <NavLink class="pr-3 text-xl" end="true" href="/">Home</NavLink>
+                <NavLink class="px-3 text-xl" end="true" href="/portfolio">Portfolio</NavLink>
+                <NavLink class="pl-3 text-xl" end="true" href="/contacts">Contacts</NavLink>
             </div>
             <div class="flex flex-row self-center">
                 <NavLink href="https://github.com/The127" target="blank"><Text class="text-3xl"><Fa icon={faGithub} /></Text></NavLink>
