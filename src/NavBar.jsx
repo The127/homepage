35174a3c2af7c1a0fc991ba5ed4350f54fd01fde
The127 <julian.baehr@googlemail.com>
2022-07-27 21:18:02 +0200

    add font awesome

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,20 +1,22 @@
 import { NavLink } from "@solidjs/router";
+import Fa from "solid-fa";
 import Text from "./Text";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function NavBar(){
     return (
         <div class="flex flex-row px-16 py-6 sticky top-0 flex-wrap justify-between items-baseline">
             <div class="flex flex-wrap flex-row items-baseline">
-                <Text class="text-xl pr-8">Karolin Bähr</Text>
-                <Text class="text text-gray-500">I'm a <i class="text-gray-700">professional</i> software developer</Text>
+                <Text class="text-3xl pr-8">Karolin Bähr</Text>
+                <Text class="text-2xl text-gray-500">I'm a <i class="text-gray-700">professional</i> software developer</Text>
             </div>
             <div>
-                <NavLink class="px-3" href="/">Home</NavLink>
-                <NavLink class="px-3" href="/portfolio">Portfolio</NavLink>
-                <NavLink class="px-3" href="/contacts">Contacts</NavLink>
+                <NavLink class="px-3 text-xl" href="/">Home</NavLink>
+                <NavLink class="px-3 text-xl" href="/portfolio">Portfolio</NavLink>
+                <NavLink class="px-3 text-xl" href="/contacts">Contacts</NavLink>
             </div>
-            <div>
-                <NavLink href="https://github.com/The127">Github</NavLink>
+            <div class="flex flex-row self-center">
+                <NavLink href="https://github.com/The127" target="blank"><Text class="text-3xl"><Fa icon={faGithub} /></Text></NavLink>
             </div>
         </div>
     );
