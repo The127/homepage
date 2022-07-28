f57cd7494154ce1811629a3e868574e709784b18
The127 <julian.baehr@googlemail.com>
2022-07-28 15:35:26 +0200

    bigger home content

--- src/HomePanelItem.jsx
+++ src/HomePanelItem.jsx
@@ -4,7 +4,7 @@ export default function HomePanelItem(props) {
     return (
         <div className="flex-1 transition-all" class={props.class}>
             <Link href={props.href} target={props.target}>
-                <div class="p-16 text-center">
+                <div class="p-16 text-center text-xl">
                     {props.children}
                 </div>
             </Link>
