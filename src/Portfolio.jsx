ae14ef7b94e50717d7b305bb6f983edb7169d4b4
The127 <julian.baehr@googlemail.com>
2022-07-28 16:07:47 +0200

    general portfolio structure

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -1,7 +1,27 @@
+import Text from "./Text";
+
 export default function Portfolio() {
     return (
-        <>
-            under construction
-        </>
+        <div class="flex flex-col container mx-auto p-3 mt-16">
+            <div class="my-3">
+                <Text class="text-6xl">Libraries</Text>
+            </div>
+            <div class="flex my-3">
+                <Text class="text-3xl">FluentBlazorRouter</Text>
+            </div>
+            <div class="flex my-3">
+                <Text class="text-3xl">HttpExceptions</Text>
+            </div>
+            <div class="flex my-3">
+                <Text class="text-3xl">Raccoon-EventHub</Text>
+            </div>
+
+            <div class="my-3">
+                <Text class="text-6xl">Projects</Text>
+            </div>
+            <div class="flex my-3">
+                <Text class="text-3xl">profyle</Text>
+            </div>
+        </div>
     );
 }
