998e48c81ecf75c87f1b76b955ac948541d73d35
The127 <julian.baehr@googlemail.com>
2022-07-28 15:34:14 +0200

    very inspiring quote

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,14 +4,18 @@ import HomePanelItem from "./HomePanelItem";
 import Text from "./Text";
 import { Link } from "@solidjs/router";
 import Fa from "solid-fa";
-import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+import { faExternalLink, faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
     return (
         <>
             <div class="container mx-auto">
-                <div class="flex flex-row flex-1 justify-center my-16 md:my-56">
+                <div class="flex flex-col flex-1 justify-center my-32 md:my-56">
+                    <div class="text-9xl top-0 mx-16 text-gray-300">
+                        <Fa icon={faQuoteLeft} />
+                    </div>
                     <Text class="text-6xl text-center huge-text">Very <i class="professional">inspiring</i> text that makes you <i class="professional">think</i>.</Text>
+                    <Text class="text-right pt-6 mr-16 text-3xl text-gray-500">- me, just now</Text>
                 </div>
             </div>
 
@@ -20,14 +24,14 @@ export default function Home() {
                     <HomePanelItem class="bg-pink-100 hover:bg-pink-200 active:bg-pink-50" href="https://liaaura.vercel.app/" target="blank">
                         <div class="flex flex-row justify-center">
                             <Text class="mr-2">
-                                i make music
+                                I make music.
                             </Text>
                             <Fa icon={faExternalLink} />
                         </div>
                     </HomePanelItem>
                     <HomePanelItem class="bg-yellow-100 hover:bg-yellow-200 active:bg-yellow-50" href="/drawings">
                         <Text>
-                            i write very bad poetry
+                            I write very bad poetry.
                         </Text>
                     </HomePanelItem>
                 </HomePanel>
@@ -35,13 +39,13 @@ export default function Home() {
                 <HomePanel>
                     <HomePanelItem class="bg-blue-100 hover:bg-blue-200 active:bg-blue-50" href="/drawings">
                         <Text>
-                            i draw sometimes
+                            I draw sometimes.
                         </Text>
                     </HomePanelItem>
                     <HomePanelItem class="bg-orange-100 hover:bg-orange-200 active:bg-orange-50" href="https://profyle.net" target="blank">
                         <div class="flex flex-row justify-center">
                             <Text class="mr-2">
-                            i made an online profile website
+                                I made an online profile website.
                             </Text>
                             <Fa icon={faExternalLink} />
                         </div>
@@ -51,12 +55,12 @@ export default function Home() {
                 <HomePanel>
                     <HomePanelItem class="bg-green-100 hover:bg-green-200 active:bg-green-50" href="/portfolio">
                         <Text>
-                            i like to code things
+                            I like to code things.
                         </Text>
                     </HomePanelItem>
                     <HomePanelItem class="bg-red-100 hover:bg-red-200 active:bg-red-50" href="/drawings">
                         <Text>
-                            i like to sleep
+                            I like to sleep.
                         </Text>
                     </HomePanelItem>
                 </HomePanel>
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -7,7 +7,7 @@ export default function NavBar() {
     return (
         <div class="navbar flex flex-row px-16 py-6 md:sticky top-0 flex-wrap justify-center md:justify-between items-center md:items-baseline drop-shadow">
             <div class="flex flex-wrap flex-row justify-center md:items-baseline w-full md:w-auto">
-                <Link href="/"><Text class="text-3xl md:pr-8 mb-3">Karolin</Text></Link>
+                <Link href="/"><Text class="text-6xl md:text-3xl md:pr-8 mb-3">Karolin</Text></Link>
                 <Text class="hidden md:block text-2xl pr-8 mb-2 text-gray-500">I'm a <i class="text-gray-700 professional">professional</i> software developer</Text>
             </div>
             <div class="md:pr-8">
