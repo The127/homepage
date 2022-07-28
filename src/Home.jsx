75a8ade8737256ea5ba7efecfcd58cab65b68ce5
The127 <julian.baehr@googlemail.com>
2022-07-28 12:14:42 +0200

    less padding on mobile for funny text

--- src/Home.jsx
+++ src/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <>
             <div class="container mx-auto">
-                <div class="flex flex-row flex-1 justify-center my-56">
+                <div class="flex flex-row flex-1 justify-center my-16 md:my-56">
                     <Text class="text-6xl text-center huge-text">Very <i class="professional">inspiring</i> text that makes you <i class="professional">think</i>.</Text>
                 </div>
             </div>
