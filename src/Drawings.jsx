d32e86d9811ae8c37889f32628ea04a84c73df75
The127 <julian.baehr@googlemail.com>
2022-07-28 16:35:26 +0200

    add basic drawings

--- src/Drawings.jsx
+++ src/Drawings.jsx
@@ -1,7 +1,8 @@
 export default function Drawings() {
     return (
-        <>
-            under construction
-        </>
+        <div class="flex flex-row flex-wrap container mx-auto p-3 mt-16">
+            <img src="src/assets/drawings/20220119_202503.jpg" width="50%" class="p-6"></img>
+            <img src="src/assets/drawings/Icon.png" width="50%" class="p-6"></img>
+        </div>
     );
 }
