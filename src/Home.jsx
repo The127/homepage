01bf984237d2a92ef85bb890c4764c61315c8db6
The127 <julian.baehr@googlemail.com>
2022-07-28 14:58:59 +0200

    add responsive effect to home panel items

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,7 @@ import HomePanel from "./HomePanel";
 import HomePanelContainer from "./HomePanelContainer";
 import HomePanelItem from "./HomePanelItem";
 import Text from "./Text";
+import { Link } from "@solidjs/router";
 
 export default function Home() {
     return (
@@ -14,12 +15,12 @@ export default function Home() {
 
             <HomePanelContainer>
                 <HomePanel>
-                    <HomePanelItem class="bg-pink-100">
+                    <HomePanelItem class="bg-pink-100 hover:bg-pink-200 active:bg-pink-50" href="https://liaaura.vercel.app/" target="blank">
                         <Text>
                             i make music
                         </Text>
                     </HomePanelItem>
-                    <HomePanelItem class="bg-yellow-100">
+                    <HomePanelItem class="bg-yellow-100 hover:bg-yellow-200 active:bg-yellow-50" href="/drawings">
                         <Text>
                             i'm kinda gay
                         </Text>
@@ -27,25 +28,25 @@ export default function Home() {
                 </HomePanel>
 
                 <HomePanel>
-                    <HomePanelItem class="bg-blue-100">
+                    <HomePanelItem class="bg-blue-100 hover:bg-blue-200 active:bg-blue-50" href="/drawings">
                         <Text>
                             i draw sometimes
                         </Text>
                     </HomePanelItem>
-                    <HomePanelItem class="bg-orange-100">
+                    <HomePanelItem class="bg-orange-100 hover:bg-orange-200 active:bg-orange-50" href="https://profyle.net" target="blank">
                         <Text>
-                            i made a online profile website
+                            i made an online profile website
                         </Text>
                     </HomePanelItem>
                 </HomePanel>
 
                 <HomePanel>
-                    <HomePanelItem class="bg-green-100">
+                    <HomePanelItem class="bg-green-100 hover:bg-green-200 active:bg-green-50" href="/drawings">
                         <Text>
                             i like to code things
                         </Text>
                     </HomePanelItem>
-                    <HomePanelItem class="bg-red-100">
+                    <HomePanelItem class="bg-red-100 hover:bg-red-200 active:bg-red-50" href="/drawings">
                         <Text>
                             i like to sleep
                         </Text>
--- src/HomePanelItem.jsx
+++ src/HomePanelItem.jsx
@@ -1,7 +1,13 @@
+import { Link } from "@solidjs/router";
+
 export default function HomePanelItem(props) {
     return (
-        <div className="flex-1 p-16" class={props.class}>
-            {props.children}
+        <div className="flex-1 transition-all" class={props.class}>
+            <Link href={props.href} target={props.target}>
+                <div class="p-16">
+                    {props.children}
+                </div>
+            </Link>
         </div>
     )
 }
