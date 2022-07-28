195e41f162e45a37e6ed4b45f9f31d217035f51a
The127 <julian.baehr@googlemail.com>
2022-07-28 12:08:31 +0200

    optimize home panel for mobile

--- src/HomePanel.jsx
+++ src/HomePanel.jsx
@@ -1,6 +1,6 @@
 export default function HomePanel(props){
     return (
-        <div class="flex flex-row flex-1">
+        <div class="flex flex-col md:flex-row flex-1">
             {props.children}
         </div>
     )
