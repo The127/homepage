c0e23a7a188a9df91657234f9ed9441136d754f0
The127 <julian.baehr@googlemail.com>
2022-07-28 13:57:32 +0200

    sassy impressum

--- src/Impressum.jsx
+++ src/Impressum.jsx
@@ -3,14 +3,14 @@ import Text from "./Text";
 export default function Impressum() {
     return (
         <>
-            <div class="container mx-auto flex flex-col">
+            <div class="container mx-auto flex flex-col p-3">
                 <Text class="text-6xl mt-12 mb-6">
                     Impressum
                 </Text>
                 <Text class="mb-3">
-                    the information you are looking for is in the footer<br />
-                    why did you even click this link?<br />
-                    are you blind?
+                    The information you are looking for is in the footer.<br />
+                    Why did you even click this link?<br />
+                    Are you blind?
                 </Text>
             </div>
         </>
